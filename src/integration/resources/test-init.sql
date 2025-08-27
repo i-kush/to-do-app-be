a11fb796e2f17e84d7b807e0072f8576dc06eab9
Ihor Kushnir <ihor.kushnir.job@gmail.com>
2025-08-27 21:43:24 +0300

    Fixed integration tests after app_user table updates

--- src/integration/resources/test-init.sql
+++ src/integration/resources/test-init.sql
@@ -1,15 +1,15 @@
 insert into tenant (id, name, created_at, updated_at)
 values (uuid_generate_v4(), 'TestTenant', now(), now());
 
-insert into app_user (id, tenant_id, username, password_hash, email, firstname, lastname, is_locked, created_at, updated_at)
+insert into app_user (id, tenant_id, role_id, username, password_hash, email, firstname, lastname, is_locked, created_at, updated_at)
 values (uuid_generate_v4(),
         (select id from tenant where name = 'TestTenant' limit 1),
+        'TENANT_ADMIN',
         'testenko',
         '$2a$10$mz2d/O6XKhkX5FOJBg3FV.m.UtNLA77CkYCOLtINukM5xKdLPHE7i',
         'testenko@test.com',
         'Test',
         'User',
         false,
-        true,
         now(),
         now());
