1ec434925f7d5ec8c6c21a61cabc808c0549bd65
Ihor Kushnir <ihor.kushnir.job@gmail.com>
2025-08-31 16:05:34 +0300

    Added request util path matcher unit test

---
+++ src/test/java/com/kush/todo/util/RequestUtilsTest.java
@@ -0,0 +1,29 @@
+package com.kush.todo.util;
+
+import com.kush.todo.config.SecurityConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class RequestUtilsTest {
+
+    @ParameterizedTest
+    @MethodSource("getIsAllowedEndpointParams")
+    void isAllowedEndpoint(String endpoint, boolean expected) {
+        boolean isAllowed = Assertions.assertDoesNotThrow(() -> RequestUtils.isAllowedEndpoint(endpoint));
+        Assertions.assertEquals(expected, isAllowed);
+    }
+
+    public static Stream<Arguments> getIsAllowedEndpointParams() {
+        return Stream.concat(
+                Stream.of(
+                        Arguments.of("/test", false),
+                        Arguments.of("/api/auth/login/anton", false)
+                ),
+                SecurityConfig.ALLOWED_ENDPOINTS.stream().map(e -> Arguments.of(e, true))
+        );
+    }
+}
